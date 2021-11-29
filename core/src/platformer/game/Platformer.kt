1c5e980936542f2a4d16930aadcf345828ccfca7
RedDime1 <dima.dima0029@gmail.com>
2021-11-29 18:45:36 +0300

    gg

--- core/src/platformer/game/Platformer.kt
+++ core/src/platformer/game/Platformer.kt
@@ -8,12 +8,16 @@ import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.utils.ScreenUtils
 import platformer.game.model.Brick
 import platformer.game.screens.GameScreen
+import platformer.game.*
+
+
 
 class Platformer : Game() {
     var game: GameScreen? = null
-
     override fun create() {
         game = GameScreen()
         setScreen(game)
     }
+    fun goHome() {}
+    override fun resize(width: Int, height: Int) {}
 }
